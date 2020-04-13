cf9e4d6342c7abfc101c8b5deb13e7b9cda31b3d
Aarchu-B <archanabaral2000@gmail.com>
2020-04-13 09:29:58 +0545

    http request and update state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from 'axios';
 import "./App.css";
 
 class App extends React.Component {
+  state={
+    users: [],
+    loading: false
+  }
+  async componentDidMount(){
+    this.setState({loading:true});
+    const res=await axios.get('https://api.github.com/users');
+   // console.log(res.data);
+   this.setState({users:res.data ,loading:false})
+
+  }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
