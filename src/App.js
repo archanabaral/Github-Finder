308a08e307e490dd6f5236c4a6c387d17a1836e7
Aarchu-B <archanabaral2000@gmail.com>
2020-04-13 14:24:19 +0545

    event and search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import axios from 'axios';
 import "./App.css";
 
@@ -10,9 +11,15 @@ class App extends React.Component {
     loading: false
   }
   async componentDidMount(){
+   // console.log(process.env.REACT_APP_GITHUB_CLIENT_ID)
+
     this.setState({loading:true});
-    const res=await axios.get('https://api.github.com/users');
+   
+    //client id and secret so that we dont run out of request
+    const res=await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
    // console.log(res.data);
+
    this.setState({users:res.data ,loading:false})
 
   }
@@ -21,6 +28,7 @@ class App extends React.Component {
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
